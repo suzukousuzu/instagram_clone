5ef50a450ca30327a377d868d75faa6cadc634bc
Kota Suzuki <addressuzuki@gmail.com>
2022-12-12 15:39:21 +0900

    add rich_text component

---
+++ lib/views/components/rich_text/rich_text_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/views/components/rich_text/link_text.dart';
+
+import 'base_text.dart';
+
+class RichTextWidget extends StatelessWidget {
+  const RichTextWidget({
+    Key? key,
+    this.styleForAll,
+    required this.texts,
+  }) : super(key: key);
+
+  final TextStyle? styleForAll;
+  final Iterable<BaseText> texts;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+          children: texts.map((baseText) {
+        if (baseText is LinkText) {
+          return TextSpan(
+              text: baseText.text,
+              //styleForAllがnullの場合はbaseTextのstyleが適用
+              style: styleForAll?.merge(baseText.style),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  baseText.onTapped();
+                });
+        } else {
+          return TextSpan(
+            text: baseText.text,
+            style: styleForAll?.merge(baseText.style),
+          );
+        }
+      }).toList()),
+    );
+  }
+}
